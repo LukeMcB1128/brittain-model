88d34ffb832ba78ff1e89c52dafed9ee2302511f
Luke Brittain <luke.brittain@gmail.com>
2026-08-04 17:39:02 -0500

    Add base vite config

---
+++ site/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useMemo } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 0
+    }}>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default App
